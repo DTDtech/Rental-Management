1310eed77302d4f476879319cd983211a4465170
DTDtech <85300403+DTDtech@users.noreply.github.com>
2023-12-03 17:25:56 +0700

    created username and password login fields

--- src/app/page.js
+++ src/app/page.js
@@ -2,8 +2,44 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      
-    </main>
+    <div className='mx-auto max-w-2xl'>
+      <form>
+        <div> 
+
+          <div className="mt-16 grid sm:grid-cols-1 space-y-5">
+            <div className="mx-auto">
+              <label htmlFor='username' className='block text-sm font-medium leading-8 text-gray-900'> Username: </label>
+              <div className="flex w-96 sm:max-w-md">
+                <input
+                  type="text"
+                  name="username"
+                  id="username"
+                  autoComplete="username"
+                  placeholder="Enter username"
+                  className='flex-1 rounded-md shadow ring-1 ring-gray-300 focus:outline-none focus-within:ring-2 focus-within:ring-inset focus-within:ring-pink-400 pl-3 sm:text-sm sm:leading-8'
+                />
+              </div>
+            </div>
+
+            <div className="mx-auto">
+              <label htmlFor='username' className='block text-sm font-medium leading-8 text-gray-900'> Password: </label>
+              <div className="flex w-96 sm:max-w-md">
+                <input
+                  type="password"
+                  name="password"
+                  id="password"
+                  autoComplete="new-password"
+                  placeholder="Enter password"
+                  className='flex-1 rounded-md shadow ring-1 ring-gray-300 focus:outline-none focus-within:ring-2 focus-within:ring-inset focus-within:ring-pink-400 pl-3 sm:text-sm sm:leading-8'
+                />
+              </div>
+            </div>
+
+          </div>
+        </div>
+      </form>
+    </div>
   )
 }
+
+
