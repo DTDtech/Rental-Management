3f1502ebafdfa0ee186693605374d8b5f72ce62b
DTDtech <85300403+DTDtech@users.noreply.github.com>
2024-03-09 17:13:25 +0700

    removed next-auth

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -1,8 +1,29 @@
+"use client";
+import React, { useRef } from "react";
+import { signIn } from "next-auth/react";
 import Image from 'next/image'
 import Logo from '../../public/Logo/92sLogo.jpg';
 
 
-export default function Home() {
+export default function LoginPage() {
+  const usernameRef = useRef("");
+  const passwordRef = useRef("");
+
+  const onSubmit = async() => {
+    const result = await signIn("credentials", {
+      username: usernameRef.current,
+      password: passwordRef.current,
+      redirect: true,
+      callbackUrl: "/",
+    })
+    .then((user) => {
+      console.log(user);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+  };
+
   return (
     <div className='mx-auto max-w-2xl'>
 
@@ -31,6 +52,10 @@ export default function Home() {
                   autoComplete="username"
                   placeholder="Enter username"
                   className='flex-1 rounded-md shadow ring-1 ring-nimbusCloud focus:outline-none focus-within:ring-2 focus-within:ring-inset focus-within:ring-atomicPink pl-3 sm:text-sm sm:leading-8'
+                  onChange={(e) => {
+                    usernameRef.current = e.target.value;
+                    console.log(usernameRef.current);
+                  }}
                 />
               </div>
             </div>
@@ -45,17 +70,23 @@ export default function Home() {
                   autoComplete="new-password"
                   placeholder="Enter password"
                   className='flex-1 rounded-md shadow ring-1 ring-nimbusCloud focus:outline-none focus-within:ring-2 focus-within:ring-inset focus-within:ring-atomicPink pl-3 sm:text-sm sm:leading-8'
+                  onChange={(e) => {
+                    passwordRef.current = e.target.value;
+                    console.log(passwordRef.current);
+                  }}
                 />
               </div>
             </div>
 
             <div className="mx-auto mt-2">
-              <button type='button' className='rounded-md shadow bg-atomicPink/50 hover:bg-lightPastelPurple focus:outline-none focus:ring-2 focus:ring-lavender px-3 py-2 text-sm text-white' 
-              href="/api/auth/login"> Sign in </button>
+              <button type='button' className='rounded-md shadow bg-atomicPink/50 hover:bg-lightPastelPurple focus:outline-none focus:ring-2 focus:ring-lavender px-3 py-2 text-sm text-white'
+              onClick={onSubmit}>
+              Log in 
+              </button>
             </div>
 
             <div className='mx-auto '>
-              <p> or sign up with </p>
+              <p> or create an account </p>
             </div>
 
           </div>
